70534608126d80c62a52ae3f1974810b5a77c68a
dattruong1203 <truongtandat9a3@outlook.com>
2022-09-24 21:10:01 +0700

    Add comments into bai2

--- bai2.rb
+++ bai2.rb
@@ -1,10 +1,10 @@
 require 'pg'
 require 'benchmark'
 require 'csv'
-require 'pry'
 
 class Bai2
-  
+
+  # Khởi tạo kết nối đến database
   @@conn = PG.connect(
     dbname: "mydatabase",
     port: 5432,
@@ -19,15 +19,23 @@ class Bai2
   end
 
 
+  # =============================
+  # Hàm generate_csv tạo file csv
+  # =============================
+  
   def generate_csv
     CSV.open("#{@table_name}.csv", "w") do |csv|
       500000.times do |i|
-        csv << [i + 1, "Steve Jobs", "unkown@mail.com", "01436597254", "Silicon valley", "24/02/1955", "The great man !!!"]
+        csv << [i + 1, "Steve Jobs #{i}", "unkown#{i}@mail.com", "01436597254", "Silicon valley", "24/02/1955", "The great man !!!"]
       end  
     end
   end
+
   
-    
+  # ========================================
+  # Hàm create_table tạo bảng trong database
+  # ========================================
+  
   def create_table
     @@conn.exec "
     CREATE TABLE IF NOT EXISTS #{@db_name} (
@@ -42,19 +50,17 @@ class Bai2
   end
   
 
-  def import_to_database_insert
-    CSV.foreach("#{@table_name}.csv", headers: true) do |row|
-      @@conn.exec("INSERT INTO test (id, name, email, phone, address, date_of_birth, profile)
-       VALUES (#{row[0]}, '#{row[1]}', '#{row[2]}', '#{row[3]}', '#{row[4]}', '#{row[5]}', '#{row[6]}')
-     ")
-    end
-  end
+  # =========================================
+  # Hàm import_to_database sẽ import file csv
+  # đã tạo vào database
+  # =========================================
   
-
   def import_to_database
 
+    # Đọc đường dẫn của file csv
     file_path = File.expand_path(File.dirname("#{@table_name}.csv")) + "/#{@table_name}.csv"
-    
+
+    # Import vào postgres thông qua việc thực thi lệnh copy
     @@conn.exec "
       COPY #{@db_name} (id, name, email, phone, address, date_of_birth, profile)
       from '#{file_path}'
@@ -63,6 +69,11 @@ class Bai2
 
   end
 
+
+  # ==========================================
+  # Hàm drop_table để xóa table trong database
+  # ==========================================
+  
   def drop_table
     @@conn.exec "
      DROP TABLE IF EXISTS #{@db_name}
@@ -76,6 +87,7 @@ ob.drop_table
 ob.generate_csv
 ob.create_table
 
+# Xuất thời gian thực thi import file csv vào database (Thời gian thực thi trên dưới 2s)
 puts Benchmark.realtime {
   ob.import_to_database
 }
