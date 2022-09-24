555e8e527320611dffe2896b7e9c2eee50367d5d
dattruong1203 <truongtandat9a3@outlook.com>
2022-09-24 09:24:17 +0700

    Done bai2

--- bai2.rb
+++ bai2.rb
@@ -1,13 +1,14 @@
 require 'pg'
 require 'benchmark'
 require 'csv'
+require 'pry'
 
 class Bai2
   
   @@conn = PG.connect(
     dbname: "mydatabase",
     port: 5432,
-    user: "myuser",
+    user: "tandat",
     password: "1111"
   )
 
@@ -20,11 +21,12 @@ class Bai2
 
   def generate_csv
     CSV.open("#{@table_name}.csv", "w") do |csv|
-      500001.times do |i|
+      500000.times do |i|
         csv << [i + 1, "Steve Jobs", "unkown@mail.com", "01436597254", "Silicon valley", "24/02/1955", "The great man !!!"]
       end  
     end
   end
+  
     
   def create_table
     @@conn.exec "
@@ -38,13 +40,27 @@ class Bai2
      profile TEXT
      );"
   end
+  
 
-  def import_to_database
+  def import_to_database_insert
     CSV.foreach("#{@table_name}.csv", headers: true) do |row|
       @@conn.exec("INSERT INTO test (id, name, email, phone, address, date_of_birth, profile)
        VALUES (#{row[0]}, '#{row[1]}', '#{row[2]}', '#{row[3]}', '#{row[4]}', '#{row[5]}', '#{row[6]}')
      ")
     end
+  end
+  
+
+  def import_to_database
+
+    file_path = File.expand_path(File.dirname("#{@table_name}.csv")) + "/#{@table_name}.csv"
+    
+    @@conn.exec "
+      COPY #{@db_name} (id, name, email, phone, address, date_of_birth, profile)
+      from '#{file_path}'
+      CSV DELIMITER ',';
+      "
+
   end
 
   def drop_table
@@ -56,17 +72,11 @@ class Bai2
 end
 
 ob = Bai2.new("posts", "test")
-# ob.drop_table
-# ob.generate_csv
-# ob.create_table
-# ob.import_to_database
+ob.drop_table
+ob.generate_csv
+ob.create_table
 
-
-puts Benchmark.measure{
-  ob.drop_table
-  ob.generate_csv
-  ob.create_table
+puts Benchmark.realtime {
   ob.import_to_database
 }
 
-puts "done"
