b51f2118d353648f891fde31da3218b4cd656ec3
dattruong1203 <truongtandat9a3@outlook.com>
2022-10-22 13:35:55 +0700

    Remove 1 unnecessary loop

--- bai7.rb
+++ bai7.rb
@@ -5,12 +5,11 @@ def find_triples(arr, sum)
   # Biến a, b, c là index duyệt qua các phần tử trong mảng arr
   a, b, c = 0, 1, 2
 
-  # Mảng arr_of_triples lưu các mảng con là bộ ba số từ mảng arr
-  # Mảng arr_of_triples_equal_target lưu các mảng con của mảng arr_of_triples mà thỏa điều kiện đề bài
-  arr_of_triples, arr_of_triples_equal_target = [], []
-
+  # Mảng arr_of_triples lưu các mảng con là bộ ba số từ mảng arr mà thỏa điều kiện đề bài
+  arr_of_triples = []
+  
   while a <= arr.length - 3
-    arr_of_triples << [arr[a], arr[b], arr[c]]
+    arr_of_triples << [arr[a], arr[b], arr[c]] if arr[a] + arr[b] + arr[c] == sum
 
     if c == arr.length - 1 && b == arr.length - 2
       a += 1
@@ -25,14 +24,7 @@ def find_triples(arr, sum)
 
   end
 
-  arr_of_triples.each do |e|
-    if e[0] + e[1] + e[2] == sum
-      arr_of_triples_equal_target << e
-    end
-  end
-
-  arr_of_triples_equal_target
-  
+  arr_of_triples
 end
 
 numbers = [12, 3, 1, 2, -6, 5, -8, 6]
